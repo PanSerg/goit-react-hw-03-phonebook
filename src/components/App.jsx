5a4a36b1417d3e85f1993496198a925264d11b70
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-01 23:15:11 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { RecipeList } from './RecipeList/RecipeList';
+// import { GlobalStyle } from './GlobalStyle';
+// import { Layout } from './Layout/Layout';
+// import initialRecipes from '../recipes.json';
+// import { RecipeForm } from './RecipeForm/RecipeForm';
+
+export class App extends Component {
+  state = {
+    recipes: [],
+  };
+  
+  componentDidMount() {
+    const savedRecipes = localStorage.getItem('recipes');
+    if (savedRecipes !== null) {
+      const parsedRecipes = JSON.parse(savedRecipes);
+      this.setState({ recipes: parsedRecipes });
+      return;
+    }
+    this.setState({ recipes: initialRecipes });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.recipe !== this.state.recipes) {
+      localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
+    }
+  };
+
+  addRecipe = newRecipe => {
+    this.setState(prevState => {
+      return {
+        recipes: [...prevState.recipes, newRecipe],
+      };
+    });
+  };
+
+  deleteRecipe = recipeId => {
+    this.setState(prevState => {
+      return {
+        recipes: prevState.recipes.filter(recipe => recipe.id !== recipeId),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <RecipeForm onSave={this.addRecipe} />
+        <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe} />
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
