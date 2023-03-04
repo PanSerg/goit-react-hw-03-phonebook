2aefdf41a407dc68f7311331f87d11c1145c2f1e
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-03-04 12:57:25 +0100

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,75 @@
 import { Component } from 'react';
-// import { RecipeList } from './RecipeList/RecipeList';
-// import { GlobalStyle } from './GlobalStyle';
-// import { Layout } from './Layout/Layout';
-// import initialRecipes from '../recipes.json';
-// import { RecipeForm } from './RecipeForm/RecipeForm';
+import Header from '.';
+import Search from '.';
 
-export class App extends Component {
-  state = {
-    recipes: [],
-  };
-  
-  componentDidMount() {
-    const savedRecipes = localStorage.getItem('recipes');
-    if (savedRecipes !== null) {
-      const parsedRecipes = JSON.parse(savedRecipes);
-      this.setState({ recipes: parsedRecipes });
-      return;
-    }
-    this.setState({ recipes: initialRecipes });
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.recipe !== this.state.recipes) {
-      localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
-    }
-  };
-
-  addRecipe = newRecipe => {
-    this.setState(prevState => {
-      return {
-        recipes: [...prevState.recipes, newRecipe],
-      };
-    });
-  };
-
-  deleteRecipe = recipeId => {
-    this.setState(prevState => {
-      return {
-        recipes: prevState.recipes.filter(recipe => recipe.id !== recipeId),
-      };
-    });
-  };
-
+class App extends Component {
   render() {
     return (
-      <Layout>
-        <RecipeForm onSave={this.addRecipe} />
-        <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe} />
-        <GlobalStyle />
-      </Layout>
-    );
+      <div className='container'>
+        <Header onShowModalLogin={this.toggleModalLogin} />
+        <Search />
+      </div>
+    )
   }
-}
+};
+
+export default App
+
+
+
+// video Repeta
+
+// import { Component } from 'react';
+// // import { RecipeList } from './RecipeList/RecipeList';
+// // import { GlobalStyle } from './GlobalStyle';
+// // import { Layout } from './Layout/Layout';
+// // import initialRecipes from '../recipes.json';
+// // import { RecipeForm } from './RecipeForm/RecipeForm';
+
+// export class App extends Component {
+//   state = {
+//     recipes: [],
+//   };
+  
+//   componentDidMount() {
+//     const savedRecipes = localStorage.getItem('recipes');
+//     if (savedRecipes !== null) {
+//       const parsedRecipes = JSON.parse(savedRecipes);
+//       this.setState({ recipes: parsedRecipes });
+//       return;
+//     }
+//     this.setState({ recipes: initialRecipes });
+//   }
+
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.recipe !== this.state.recipes) {
+//       localStorage.setItem('recipes', JSON.stringify(this.state.recipes));
+//     }
+//   };
+
+//   addRecipe = newRecipe => {
+//     this.setState(prevState => {
+//       return {
+//         recipes: [...prevState.recipes, newRecipe],
+//       };
+//     });
+//   };
+
+//   deleteRecipe = recipeId => {
+//     this.setState(prevState => {
+//       return {
+//         recipes: prevState.recipes.filter(recipe => recipe.id !== recipeId),
+//       };
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <Layout>
+//         <RecipeForm onSave={this.addRecipe} />
+//         <RecipeList items={this.state.recipes} onDelete={this.deleteRecipe} />
+//         <GlobalStyle />
+//       </Layout>
+//     );
+//   }
+// }
